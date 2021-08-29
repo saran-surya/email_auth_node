8920fde81820370e16606acd2be4f5066c8f56f2
Saran Surya <saransurya199@gmail.com>
2021-08-29 22:33:06 +0530

    Configurations made

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+// Server essentials
+const cors = require('cors');
+const helmet = require('helmet');
+
+// environment configurations
+const dotenv = require('dotenv');
+dotenv.config({
+    path: path.resolve("../", "custom", "config.env")
+});
+
+
+// server configurations
+
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// router for the server
+const router = require('./routes/route');
+app.use('/', router);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log('server started');
+    console.log('port', port);
+});
---
+++ server/routes/route.js
@@ -0,0 +1,159 @@
+const crypto = require("crypto");
+
+// creating the router
+const express = require('express');
+const router = express.Router();
+
+// spawn for sending the Mails
+const { spawn } = require('child_process');
+
+// environment configuration
+const dotenv = require('dotenv');
+const path = require("path");
+dotenv.config({
+    path: path.resolve(__dirname, "../", "../", "custom", "config.env")
+});
+
+
+let mailData = "";
+
+
+/**
+ * Returns a hashed string value : encodes SHA256
+ * @param {String} data : Proceed with a string 
+ * @returns {String} Hashed string
+ */
+function sha256(data) {
+    return crypto.createHash("sha256").update(data, "binary").digest("base64");
+}
+
+/**
+ * This function generates a OTP of min length (3) and max length (6)
+ * @param {Number} len The length of the OTP, defaults : max = 6 : min = 3
+ * @returns {String} A OTP number of desired length 
+ */
+function generateOtp(len = 6) {
+    let otp = String(Math.floor(100000 + Math.random() * 900000));
+    return ((len <= 6 && len > 2) ? otp.slice(0, len) : otp)
+}
+
+/**
+ * This function validates if the provided string is a valid email ID
+ * @param {String} email 
+ * @returns {Boolean}
+ */
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function checkServerKey(serverKey) {
+    if (!serverKey) {
+        return false
+    }
+    return (sha256(serverKey) == process.env.SERVER_API_KEY)
+}
+
+
+router.get("/test/fail", async (req, res) => {
+    res.json({
+        status: 404,
+        mail: "test Phase",
+        error: "The mail id is not valid",
+        success: false
+    });
+})
+
+
+router.get("/test/dart", async (req, res) => {
+    res.status(200).json({
+        "message": "success"
+    })
+})
+
+router.get('/', async (req, res) => {
+    res.send("Hello Welcome to the homepage");
+})
+
+
+// Takes care of sending the mails
+router.get("/dart/auth/:mail", async (req, res) => {
+    try {
+        const { mail } = req.params;
+
+        // Handle the server verification
+        const { key, otpLength } = req.query;
+        let { CompanyName } = req.query;
+
+        
+        console.log("-------- Server Key Verification ---------")
+        // console.log(key)
+        // console.log(sha256(key))
+        // console.log(process.env.SERVER_API_KEY)
+        console.log(checkServerKey(key))
+        console.log("------------------------------------------")
+
+        if (!checkServerKey(key)) {
+            res.status(404).json({
+                success: false,
+                message: "Server Key does not match"
+            })
+            console.log(":: Server Key does not match")
+            return
+        }
+
+       
+
+        if (CompanyName == undefined || CompanyName.length <= 0) {
+            CompanyName = "";
+        }
+
+        if (validateEmail(mail) && mail.length > 0) {
+            let OTP = generateOtp(otpLength ? otpLength : null);
+            // console.log(path.resolve(__dirname, "../", 'mailer.py'))
+            // return false
+            mailData = spawn('python', [path.resolve(__dirname, "../", 'mailer.py'), 'dart', mail, OTP, CompanyName])
+            mailData.stdout.on('data', async (data) => {
+                const readerData = await data.toString();
+                console.log(data.toString());
+                if (readerData.includes('error')) {
+                    res.json({
+                        status: 404,
+                        mail: mail,
+                        success: false,
+                        message: "Unable to send mail"
+                    })
+                } else {
+                    res.json({
+                        status: 200,
+                        mail: mail,
+                        OTP: OTP,
+                        success: true
+                    });
+                }
+            })
+        } else {
+            res.json({
+                status: 404,
+                mail: mail,
+                error: "The mail id is not valid",
+                success: false
+            });
+        }
+    } catch (error) {
+        console.log(error)
+        console.log(error.message);
+        res.json({
+            status: 404,
+            error: "Server error",
+            success: false
+        });
+    }
+})
+
+
+
+
+
+module.exports = router
